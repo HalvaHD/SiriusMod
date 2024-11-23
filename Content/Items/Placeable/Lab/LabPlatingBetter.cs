8dfc939b702d5c92f264a22e99df1c3300b83965
Ink of the Eternity <error34150@gmail.com>
2024-11-23 21:35:43 +0300

    added lab tiles

---
+++ Content/Items/Placeable/Lab/LabPlatingBetter.cs
@@ -0,0 +1,26 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ProtoMod.Content.Items.Placeable.Lab
+{
+    public class LabPlatingBetter : ModItem
+    {
+        public override void SetStaticDefaults()
+            => Item.ResearchUnlockCount = 100;
+
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = Terraria.Item.CommonMaxStack;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createTile = Mod.Find<ModTile>(GetType().Name).Type;
+        }
+    }
+}
