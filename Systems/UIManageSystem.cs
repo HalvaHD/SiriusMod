cc79de9815dac160e14ae462908af620d41e1f26
Ink of the Eternity <error34150@gmail.com>
2025-03-07 00:31:34 +0300

    Completely rebuild Overheat UI System

---
+++ Systems/UIManageSystem.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using SiriusMod.UI.Mechanics;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.UI;
+
+namespace SiriusMod.Systems
+{
+    public class UIManageSystem : ModSystem
+    {
+        // public static Vector2 PreviousMouseWorld;
+        //
+        // public static Vector2 PreviousZoom;
+
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+        {
+            int buffDisplayIndex = layers.FindIndex(layer => layer.Name == "Vanilla: Resource Bars");
+            if (buffDisplayIndex != -1)
+            {
+            }
+
+            int Index = layers.FindIndex(layer => layer.Name == "Vanilla: Mouse Text");
+            if (Index != -1)
+            {
+
+                // Sulphuric water poison bar
+                layers.Insert(Index, new LegacyGameInterfaceLayer("Overheat UI", () =>
+                {
+                    OverheatUI.Draw(Main.spriteBatch, Main.LocalPlayer);
+                    return true;
+                }, InterfaceScaleType.None));
+            }
+
+
+            int invasionIndex = layers.FindIndex(layer => layer.Name == "Vanilla: Diagnose Net");
+            if (invasionIndex != -1)
+            {
+            }
+        }
+    }
+}
--- UI/Mechanics/OverheatUI.cs
+++ UI/Mechanics/OverheatUI.cs
@@ -1,7 +1,10 @@
+using System;
 using SiriusMod.Mechanics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
+using CalamityMod;
+using Microsoft.Xna.Framework.Input;
 using ReLogic.Content;
 using Terraria;
 using Terraria.GameContent;
@@ -13,187 +16,111 @@ using Terraria.UI;
 
 namespace SiriusMod.UI.Mechanics
 {
-    internal class OverheatUI : UIState
+    // Как выяснилось все гораздо проще делеать через ModSystem
+    public class OverheatUI : ModSystem
     {
-        private UIElement area;
-        public static UIImage frame;
-        private BarLine progressBar;
-
-        public override void OnInitialize()
-        {
-            // Инициализация элемента
-            area = new UIElement();
-            // Отступ от левого края в пикселях/процентах
-            area.Left.Set(-18, 0.5f);
-            // Оступ от верхнего края в пикселя/процентах
-            area.Top.Set(0, 0.55f);
-            // Размер области UI, роляет ток где надо на что-то кликать
-            area.Width.Set(36, 0f);
-            area.Height.Set(12, 0f);
-
-            frame = new UIImage(ModContent.Request<Texture2D>("SiriusMod/Assets/ExtraTextures/Kitkat"));
-            frame.Left.Set(0, 0f);
-            frame.Top.Set(0, 0f);
-            frame.ImageScale = 1.5f;
-            //frame.Width.Set(36, 0f);
-            //frame.Height.Set(12, 0f);
-
-            progressBar = new BarLine();
-            progressBar.Left.Set(0, 0f);
-            progressBar.Top.Set(0, 0f);
-
-            //К основной области добавляешь все элементы, которые должны "крепиться" к ней
-            area.Append(frame);
-            frame.Append(progressBar);
-            // Добавляешь саму область на экран
-            Append(area);
-        }
-
-        // Отрисовка UI, чтобы ее отменить возращаем return;
-        public override void Draw(SpriteBatch spriteBatch)
-        {
-            if (Main.LocalPlayer.HeldItem.ModItem is not Overheat overheatItem) // && !Main.LocalPlayer.controlUseItem
-            {
-                return;
-            }
+        // На будущее
+        private const float MouseDragEpsilon = 0.05f; // 0.05%
         
-            base.Draw(spriteBatch);
+        // Дефолтная позиция под игроком
+        internal const float DefaultPosX = 50.104603f;
+        internal const float DefaultPosY = 55.765408f;
+        
+        // На будущее
+        private static Vector2? dragOffset = null;
+        private static Texture2D frameTexture, barTexture;
+        public override void OnModLoad()
+        {
+            frameTexture = ModContent.Request<Texture2D>("SiriusMod/UI/Mechanics/Kitkat", AssetRequestMode.ImmediateLoad).Value;
+            barTexture = ModContent.Request<Texture2D>("SiriusMod/UI/Mechanics/Kitkat_Bar", AssetRequestMode.ImmediateLoad).Value;
+            dragOffset = null;
         }
 
-        protected override void DrawSelf(SpriteBatch spriteBatch)
+        public override void Unload()
         {
-            base.DrawSelf(spriteBatch);
-            if (Main.LocalPlayer.HeldItem.ModItem is not Overheat overheatItem) // && !Main.LocalPlayer.controlUseItem
+            dragOffset = null;
+            frameTexture = barTexture = null;
+        }
+        
+        // Сам метод активируется в файлике UIManageSystem.cs
+        public static void Draw(SpriteBatch spriteBatch, Player player)
+        {
+            if (Main.LocalPlayer.HeldItem.ModItem is not Overheat overheatitem)
             {
                 return;
             }
-            // Rectangle progressrect = frame.GetInnerDimensions().ToRectangle();
-            // progressrect.Width = (int)(36 * MathHelper.Clamp((overheatItem.OverheatLevel / 420f), 0f, 1f));
-            // Main.NewText($"Width - {progressrect.Width}");
-            // spriteBatch.Draw((ModContent.Request<Texture2D>("SiriusMod/Assets/ExtraTextures/Kitkat_Bar").Value), new Rectangle(750, 464, 36, 12), Color.White);
-            // Main.NewText(new Rectangle(progressrect.Left + 1, progressrect.Y, 1, progressrect.Height));
-            //
-        }
-    }
-
-
-    public class BarLine : UIElement
-    {
-        public override void Draw(SpriteBatch spriteBatch)
-        {
-            base.Draw(spriteBatch);
+            Main.NewText("Отрисовка идет");
+            Vector2 screenRatioPosition = new Vector2(DefaultPosX, DefaultPosY); // TODO: Add config pos
             
-            if (Main.LocalPlayer.HeldItem.ModItem is not Overheat overheatItem) // && !Main.LocalPlayer.controlUseItem
-            {
-                return;
-            }
-            Rectangle progressrect = OverheatUI.frame.GetInnerDimensions().ToRectangle();
-            progressrect.Width = (int)(36 * MathHelper.Clamp((overheatItem.OverheatLevel / 420f), 0f, 1f));
-            Main.NewText($"Width - {progressrect.Width}");
-            spriteBatch.Draw((ModContent.Request<Texture2D>("SiriusMod/Assets/ExtraTextures/KitKat_Bar").Value), new Vector2(progressrect.Left + 20, progressrect.Y + 20), new Rectangle?(new Rectangle(0, 0, progressrect.Width, 12)), Color.White, 0f,
-                (new Rectangle(0, 0, progressrect.Width, 12)).Size() / 2f, 1.5f, SpriteEffects.None, 0f);
-            Main.NewText(new Rectangle(progressrect.Left + 1, progressrect.Y, 1, progressrect.Height));
+            float uiScale = Main.UIScale;
+            Vector2 screenPos = screenRatioPosition;
+            screenPos.X = (int)(screenPos.X * 0.01f * Main.screenWidth);
+            screenPos.Y = (int)(screenPos.Y * 0.01f * Main.screenHeight);
             
-        }
-    }
-    
-    // gpt
-    // public class UIImageBar : UIElement
-    // {
-    //     private Texture2D _texture;
-    //     public float FillPercent = 1f; // от 0 до 1
-    //     public float ImageScale { get; set; } = 1f;
-    //
-    //     public UIImageBar(Texture2D texture)
-    //     {
-    //         _texture = texture;
-    //         // Задаём элементу размеры, чтобы не были нулевыми
-    //         Width.Set(texture.Width, 0f);
-    //         Height.Set(texture.Height, 0f);
-    //     }
-    //
-    //     protected override void DrawSelf(SpriteBatch spriteBatch)
-    //     {
-    //         base.DrawSelf(spriteBatch);
-    //
-    //         // Берём фактические размеры, которые занял UIElement на экране
-    //         var dimensions = GetDimensions().ToRectangle();
-    //
-    //         // Ширина "заполненной" части текстуры
-    //         int fillWidth = (int)(_texture.Width * FillPercent);
-    //         // Защита от дурака, если fillWidth < 0
-    //         if (fillWidth < 0)
-    //             fillWidth = 0;
-    //         if (fillWidth > _texture.Width)
-    //             fillWidth = _texture.Width;
-    //
-    //         // Исходная часть текстуры, которую рисуем
-    //         Rectangle sourceRect = new Rectangle(0, 0, fillWidth, _texture.Height);
-    //
-    //         // Куда рисуем: ту же ширину, но учитываем масштаб
-    //         Rectangle destinationRect = new Rectangle(
-    //             dimensions.X,
-    //             dimensions.Y,
-    //             (int)(fillWidth * ImageScale),
-    //             (int)(_texture.Height * ImageScale)
-    //         );
-    //
-    //         // Рисуем только кусок текстуры
-    //         spriteBatch.Draw(_texture, destinationRect, sourceRect, Color.White);
-    //     }
-    // }
+            // Здесь отрисовываем шкалу
+            DrawOverheatBar(spriteBatch, overheatitem, screenPos);
 
-    
-    
-    
-    //Система для отрисовки UI, происходит только на стороне клиента, так как любая отрисовка только на стороне клиента!!!
-    [Autoload(Side = ModSide.Client)]
-    internal class OverheatUISystem : ModSystem
-    {
-        // Интерфейс игрока многослойный, поэтому чтобы нарисовать UI мы создаем еще один слой на котором будет рисоваться шкала
-        private UserInterface OverheatUIUserInterface;
+            Rectangle mouseHitbox = new Rectangle((int)Main.MouseScreen.X, (int)Main.MouseScreen.Y, 8, 8);
+            Rectangle OverheatBarArea = Utils.CenteredRectangle(screenPos, frameTexture.Size() * uiScale);
 
-        // Наследуем класс нашего UIState, который находится выше
-        internal OverheatUI OverheatUI;
+            MouseState ms = Mouse.GetState();
+            Vector2 mousePos = Main.MouseScreen;
 
-        public override void Load()
-        {
-            // При загрузке мода загружаем наш UiState и UserInterface
-            OverheatUI = new();
-            OverheatUIUserInterface = new();
-            // Так как весь слой существует ради шкалы, то устанавливает состояние слоя на эту шкалу, чтобы слой существовал для нее
-            OverheatUIUserInterface.SetState(OverheatUI);
-        }
-
-        // Интерфейс обновляет каждый тик, поэтому нам важно обновлять интерфейс каждый тик для взаимодействия и каких-либо анимаций, если будут
-        public override void UpdateUI(GameTime gameTime)
-        {
-            OverheatUIUserInterface?.Update(gameTime);
-        }
-
-        // Чтобы встроить наш слой в кучу других слоев мы используем метод модификации слоев интерфейса
-        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
-        {
-            // Здесь мы ищемм индекс слоя интерфейса в Террарии для ванильных ресурс  баров
-            int resourceBarIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Resource Bars"));
-            // Если нашелся (То, чтобы не нашелся такого быть не может)
-            if (resourceBarIndex != -1)
+            // При пересечении
+            if (OverheatBarArea.Intersects(mouseHitbox))
             {
-                // Вставляем в слои, наш слой
-                layers.Insert(resourceBarIndex, new LegacyGameInterfaceLayer(
-                    "SiriusMod: Overheat",
-                    // Через delegate рисуем сам интерфейс
-                    delegate
-                    {
-                        OverheatUIUserInterface.Draw(Main.spriteBatch, new GameTime());
-                        
-                        return true;
-                    },
-                    // Здесь применяется тип скейла, в нашем случае - скейл интерфейс
-                    // Попробуй заменить UI на Game, чтобы посмотреть если твоя плашка апскейлится вместе с игрой, а не интерфейсом
-                    InterfaceScaleType.UI)
-                );
+                // if (!SiriusConfig.Instance.LockedOverheatUI)
+                //     Main.LocalPlayer.mouseInterface = true;
+
+                // If the mouse is on top of the meter, show the player's accumulated sulphuric poisoning.
+                if (overheatitem.OverheatLevel > 0)
+                {
+                    string overheatText = (overheatitem.OverheatLevel / 420f * 100f).ToString("n2");
+                    Main.instance.MouseText(overheatText, 0, 0, -1, -1, -1, -1);
+                }
+
+                Vector2 newScreenRatioPosition = screenRatioPosition;
+
+                // TODO: Add config moveability
+                // if (!SiriusConfig.Instance.MeterPosLock && ms.LeftButton == ButtonState.Pressed)
+                // {
+                //     // If the drag offset doesn't exist yet, create it.
+                //     if (!dragOffset.HasValue)
+                //         dragOffset = mousePos - screenPos;
+                //
+                //     // Given the mouse's absolute current position, compute where the corner of the water bar should be based on the original drag offset.
+                //     Vector2 newCorner = mousePos - dragOffset.GetValueOrDefault(Vector2.Zero);
+                //
+                //     // Convert the new corner position into a screen ratio position.
+                //     newScreenRatioPosition.X = (100f * newCorner.X) / Main.screenWidth;
+                //     newScreenRatioPosition.Y = (100f * newCorner.Y) / Main.screenHeight;
+                // }
+
+                // Compute the change in position. If it is large enough, actually move the meter
+                Vector2 delta = newScreenRatioPosition - screenRatioPosition;
+                if (Math.Abs(delta.X) >= MouseDragEpsilon || Math.Abs(delta.Y) >= MouseDragEpsilon)
+                {
+                    // newScreenRatioPosition.X
+                    // newScreenRatioPosition.Y
+                }
+
+                // When the mouse is released, save the config and destroy the drag offset.
+                if (ms.LeftButton == ButtonState.Released)
+                {
+                    dragOffset = null;
+                }
             }
+        }
+        
+        private static void DrawOverheatBar(SpriteBatch spriteBatch, Overheat OverheatItem, Vector2 screenPos)
+        {
+            float uiScale = Main.UIScale;
+            float offset = (frameTexture.Width - barTexture.Width) * 0.5f;
+            spriteBatch.Draw(frameTexture, screenPos,new Rectangle(0, 0, (frameTexture.Width), frameTexture.Height), Color.White , 0f, frameTexture.Size() * 0.5f, uiScale, SpriteEffects.None, 0);
+            Main.NewText("Работаем");
+            float completionRatio = MathHelper.Clamp(OverheatItem.OverheatLevel / 420f, 0f, 1f);
+            Rectangle barRectangle = new Rectangle(0, 0, (int)(barTexture.Width * completionRatio), barTexture.Width);
+            spriteBatch.Draw(barTexture, screenPos + new Vector2(offset * uiScale, 0), barRectangle, Color.White, 0f, frameTexture.Size() * 0.5f, uiScale, SpriteEffects.None, 0);
         }
     }
 }
